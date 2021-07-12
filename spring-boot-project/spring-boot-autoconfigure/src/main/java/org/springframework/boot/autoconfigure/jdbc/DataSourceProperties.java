cd3921240b883aa002d2153c51c99f3111e2679f
lily <lily@nihility.cn>
2021-07-12 15:39:40 +0800

    add spring boot tomcat test project

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
@@ -222,7 +222,6 @@ public class DataSourceProperties implements BeanClassLoaderAware, InitializingB
 	public String determineDriverClassName() {
 		if (StringUtils.hasText(this.driverClassName)) {
 			Assert.state(driverClassIsLoadable(), () -> "Cannot load driver class: " + this.driverClassName);
-			return this.driverClassName;
 		}
 		String driverClassName = null;
 		if (StringUtils.hasText(this.url)) {
---
+++ spring-boot-tests/spring-boot-nihility-tests/spring-boot-nihility-test-tomcat/src/main/java/cn/nihility/boot/tomcat/ApplicationTomcatStarter.java
@@ -0,0 +1,13 @@
+package cn.nihility.boot.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTomcatStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTomcatStarter.class, args);
+    }
+
+}
